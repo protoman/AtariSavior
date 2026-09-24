487be2f3196432cc275e4a809468c43ba77aa115
Iuri Fiedoruk <protoman@upperland.net>
2026-09-24 11:23:41 -0300

    L2 room2 passage, models grid, TODO lamps

--- src/generated/level_002_room_002.asm
+++ src/generated/level_002_room_002.asm
@@ -4,11 +4,11 @@ L2R2RoomRects:
   .byte 0, 0, 18, 4  ; x, y, width, height
   .byte 0, 4, 1, 4  ; x, y, width, height
   .byte 0, 8, 8, 4  ; x, y, width, height
-  .byte 9, 8, 11, 4  ; x, y, width, height
+  .byte 11, 8, 7, 4  ; x, y, width, height
   .byte 0              ; number of hot rectangles
 L2R2TilePF0:
   .byte $f0, $f0, $f0, $f0, $10, $10, $10, $10, $f0, $f0, $f0, $f0
 L2R2TilePF1:
-  .byte $ff, $ff, $ff, $ff, $00, $00, $00, $00, $f7, $f7, $f7, $f7
+  .byte $ff, $ff, $ff, $ff, $00, $00, $00, $00, $f1, $f1, $f1, $f1
 L2R2TilePF2:
-  .byte $3f, $3f, $3f, $3f, $00, $00, $00, $00, $ff, $ff, $ff, $ff
+  .byte $3f, $3f, $3f, $3f, $00, $00, $00, $00, $3f, $3f, $3f, $3f
